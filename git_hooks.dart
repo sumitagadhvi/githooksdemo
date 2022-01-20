3f0e33070fcab1a5a29c5b0ab84fff13af1e247a
Sumita Naiya <sumita.naiya@creolestudios.com>
2022-01-20 13:37:26 +0530

    fix:submit information

--- git_hooks.dart
+++ git_hooks.dart
@@ -5,16 +5,23 @@ import "package:git_hooks/git_hooks.dart";
 
 void main(List<String> arguments) {
   final params = {
+    Git.preCommit: _preCommit,
     Git.commitMsg:commitMsg,
   };
   GitHooks.call(arguments, params);
+}
+
+Future<bool> _preCommit() async {
+ var hooks = await Hooks.create();  // adjust behaviour if neccessary
+  final result = await hooks();  // run activated hooks on staged files
+  return result.isSuccess;  // report the result
 }
 Future<bool> commitMsg() async {
   var commitMsg = Utils.getCommitEditMsg();
   if (commitMsg.startsWith('fix:')) {
     return true; // you can return true let commit go
   } else  {
-    print('you should add `fix` in the commit message');
+    print('you should add `fix` in the commit message before commit');
     return false;
   }
 }
